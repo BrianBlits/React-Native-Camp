ccac5a6abdf160770d859359075b2ffacfa8d4f2
Blits <brianlapeyrouse98@gmail.com>
2021-11-17 16:27:54 -0600

    Using Redux

--- components/AboutComponent.js
+++ components/AboutComponent.js
@@ -1,7 +1,15 @@
 import React, { Component } from 'react';
 import { ScrollView, Text, FlatList } from 'react-native';
-import { PARTNERS } from '../shared/partners';
 import { Card, ListItem } from 'react-native-elements';
+import { connect } from 'react-redux';
+import { baseUrl } from '../shared/baseUrl';
+
+
+const mapStateToProps = state => {
+    return {
+        partners: state.partners
+    };
+};
 
 function Mission(){
     return(
@@ -16,12 +24,6 @@ function Mission(){
 }
 
 class About extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            partners: PARTNERS
-        };
-    }
 
     static navigationOptions = {
         title: 'About Us'
@@ -33,7 +35,7 @@ class About extends Component {
                 <ListItem
                     title={item.name}
                     subtitle={item.description}
-                    leftAvatar={{ source: require('./images/bootstrap-logo.png')}}
+                    leftAvatar={{source: {uri: baseUrl + item.image}}}
                 />
             );
         };
@@ -43,15 +45,15 @@ class About extends Component {
                 <Card 
                     title={'Community Partners'}
                     wrapperStyle={{margin: 20}}>
-                        <FlatList
-                            data={this.state.partners}
-                            renderItem={renderPartner}
-                            keyExtractor={item => item.id.toString()}
-                        />
+                        <FlatList 
+                        data={this.props.partners.partners}
+                        renderItem={renderPartner}
+                        keyExtractor={item => item.id.toString()}
+                    />
                 </Card>
             </ScrollView>
         );
     }
 }
 
-export default About;
+export default connect(mapStateToProps)(About);
