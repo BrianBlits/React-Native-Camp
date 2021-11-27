82a0c9d5d1d1df13df5c273e0a97e9e40d6b330e
Blits <brianlapeyrouse98@gmail.com>
2021-11-27 08:43:35 -0600

    Animatable

--- components/ContactComponent.js
+++ components/ContactComponent.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import { ScrollView, Text } from 'react-native';
 import { Card } from 'react-native-elements';
+import * as Animatable from 'react-native-animatable';
 
 class Contact extends Component{
     static navigationOptions = {
@@ -10,23 +11,25 @@ class Contact extends Component{
     render(){
         return(
             <ScrollView>
-                <Card>
-                    <Text style={{marginBottom:10}}>
-                        1 Nucamp Way
-                    </Text>
-                    <Text style={{marginBottom:10}}>
-                        Seattle, WA 98001
-                    </Text>
-                    <Text style={{marginBottom:10}}>
-                        U.S.A.
-                    </Text>
-                    <Text style={{marginBottom:10}}>
-                        Phone: 1-206-555-1234
-                    </Text>
-                    <Text style={{marginBottom:10}}>
-                        Email: campsites@nucamp.co
-                    </Text>
-                </Card>
+                <Animatable.View animation='fadeInDown' duration={2000} delay={1000}>
+                    <Card>
+                        <Text style={{marginBottom:10}}>
+                            1 Nucamp Way
+                        </Text>
+                        <Text style={{marginBottom:10}}>
+                            Seattle, WA 98001
+                        </Text>
+                        <Text style={{marginBottom:10}}>
+                            U.S.A.
+                        </Text>
+                        <Text style={{marginBottom:10}}>
+                            Phone: 1-206-555-1234
+                        </Text>
+                        <Text style={{marginBottom:10}}>
+                            Email: campsites@nucamp.co
+                        </Text>
+                    </Card>
+                </Animatable.View>
             </ScrollView>
         );
     }
