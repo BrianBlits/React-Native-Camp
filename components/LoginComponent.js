1f2126c8f49458066a34b3cdf569b9fb4c3ff2ed
Blits <brianlapeyrouse98@gmail.com>
2021-12-06 07:36:30 -0600

    Week Four Assignment

--- components/LoginComponent.js
+++ components/LoginComponent.js
@@ -6,15 +6,7 @@ import * as ImagePicker from 'expo-image-picker';
 import * as Permissions from 'expo-permissions';
 import { createBottomTabNavigator } from 'react-navigation-tabs';
 import { baseUrl } from '../shared/baseUrl';
-
-import React, { Component } from 'react';
-import { View, StyleSheet, ScrollView, Image } from 'react-native';
-import { Input, CheckBox, Button, Icon } from 'react-native-elements';
-import * as SecureStore from 'expo-secure-store';
-import * as ImagePicker from 'expo-image-picker';
-import * as Permissions from 'expo-permissions';
-import { createBottomTabNavigator } from 'react-navigation-tabs';
-import { baseUrl } from '../shared/baseUrl';
+import * as ImageManipulator from 'expo-image-manipulator';
 
 class LoginTab extends Component {
 
@@ -159,19 +151,42 @@ class RegisterTab extends Component {
 
     getImageFromCamera = async () => {
         const cameraPermission = await Permissions.askAsync(Permissions.CAMERA);
-        const cameraRollPermission = await Permissions.askAsync(Permissions.CAMERA_ROLL);
-
-        if (cameraPermission.status === 'granted' && cameraRollPermission.status === 'granted') {
-            const capturedImage = await ImagePicker.launchCameraAsync({
-                allowsEditing: true,
-                aspect: [1, 1]
-            });
-            if (!capturedImage.cancelled) {
-                console.log(capturedImage);
-                this.setState({imageUrl: capturedImage.uri});
-            }
+        const cameraRollPermission = await Permissions.askAsync(
+          Permissions.CAMERA_ROLL
+        );
+        if (
+          cameraPermission.status === "granted" &&
+          cameraRollPermission.status === "granted"
+        ) {
+          const capturedImage = await ImagePicker.launchCameraAsync({
+            allowsEditing: true,
+            aspect: [1, 1],
+          });
+          if (!capturedImage.cancelled) {
+            console.log(capturedImage);
+            this.processImage(capturedImage.uri);
+          }
         }
-    }
+      };
+      processImage = async (imgUri) => {
+        const processedImage = await ImageManipulator.manipulateAsync(
+          imgUri, [{resize: {width: 400}}], { format: "png" }
+        );
+        console.log("processedImage", processedImage);
+        this.setState({imageUrl: processedImage.uri});
+      }
+
+      getImageFromGallery = async () => {
+          const cameraRollPermissions = await Permissions.askAsync(Permissions.CAMERA_ROLL);
+          const capturedImage = await ImagePicker.launchImageLibraryAsync({
+            allowsEditing: true,
+            aspect: [1, 1],
+          });
+          if (!capturedImage.cancelled) {
+            console.log(capturedImage);
+            this.processImage(capturedImage.uri);
+          }
+      }
 
     handleRegister() {
         console.log(JSON.stringify(this.state));
@@ -199,6 +214,10 @@ class RegisterTab extends Component {
                         <Button
                             title='Camera'
                             onPress={this.getImageFromCamera}
+                        />
+                        <Button
+                            title='Gallery'
+                            onPress={this.getImageFromGallery}
                         />
                     </View>
                     <Input
